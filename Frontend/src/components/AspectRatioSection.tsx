799ca6879433b35a4a17ca73a8062b1c5c1bb5c5
LokeshOtti29 <lokeshotti456@gmail.com>
2026-01-27 23:55:18 +0530

    Adding aspectratioselector

---
+++ Frontend/src/components/AspectRatioSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AspectRatioSection = () => {
+  return (
+    <div className='space-y-3 dark'>
+      <label className='block text-sm font-medium text-zinc-200'>Aspect Ratio</label>
+      <div className='flex flex-wrap gap-2'> 
+
+      </div>
+    </div>
+  )
+}
+
+export default AspectRatioSection
